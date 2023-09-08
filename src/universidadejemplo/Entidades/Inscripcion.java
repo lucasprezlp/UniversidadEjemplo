ca4b10410fa741cba21240ea3176133c99585250
VeroHerLabarden <verochernandez1130@gmail.com>
2023-09-08 15:40:49 -0300

    actualice clase materia

--- src/universidadejemplo/Entidades/Inscripcion.java
+++ src/universidadejemplo/Entidades/Inscripcion.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadejemplo.Entidades;
 
-/**
- *
- * @author lucas
- */
 public class Inscripcion {
     
 }
--- src/universidadejemplo/Entidades/Materia.java
+++ src/universidadejemplo/Entidades/Materia.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadejemplo.Entidades;
 
-/**
- *
- * @author lucas
- */
+
 public class Materia {
+        public int idMateria;
+    public String nombre;
+    public int anio; 
+    public boolean activo;
+
+    public Materia(int idMateria, String nombre, int anio, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.activo = activo;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anio=" + anio + ", activo=" + activo + '}';
+    }
     
 }
