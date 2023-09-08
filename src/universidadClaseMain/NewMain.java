d3ccfbaabcee4d47b0524c780a22a4ad7484af5d
Cisc0Matic <agusbarriobarrio87@gmail.com>
2023-09-08 17:53:53 -0300

    8/9/23 commit final

--- src/universidadClaseMain/NewMain.java
+++ src/universidadClaseMain/NewMain.java
@@ -2,12 +2,21 @@
 package universidadClaseMain;
 
 import java.sql.Connection;
+import java.time.LocalDate;
+import universidadejemplo.Entidades.Alumno;
+import universidadejemploAccesoADAtos.AlumnoData;
 import universidadejemploAccesoADAtos.Conexion;
 
 public class NewMain {
 
     public static void main(String[] args) {
       Connection con= Conexion.getConexion();
+      
+      //probar
+      Alumno a1 = new Alumno(1234567, "suarez", "juana", LocalDate.of(2001,05,25), true );
+      AlumnoData alu = new AlumnoData();
+      alu.guardarAlumno(a1);
+      
     }
     
 }
