abf9d88c0233ca129c7978f42a8f5cd327e98d9f
Cisc0Matic <agusbarriobarrio87@gmail.com>
2023-09-10 13:27:08 -0300

    Metodo ModificarAlumno resuelto

--- src/universidadClaseMain/NewMain.java
+++ src/universidadClaseMain/NewMain.java
@@ -1,4 +1,3 @@
-
 package universidadClaseMain;
 
 import java.sql.Connection;
@@ -10,13 +9,17 @@ import universidadejemploAccesoADAtos.Conexion;
 public class NewMain {
 
     public static void main(String[] args) {
-      Connection con= Conexion.getConexion();
-      
-      //probar
-      Alumno a1 = new Alumno(1234567, "suarez", "juana", LocalDate.of(2001,05,25), true );
-      AlumnoData alu = new AlumnoData();
-      alu.guardarAlumno(a1);
-      
+        Connection con = Conexion.getConexion();
+
+        //probar
+        AlumnoData alu = new AlumnoData();
+        Alumno a1 = new Alumno(1234567, "suarez", "carlos", LocalDate.of(2001, 05, 25), true);
+        // Usar este constructor de alumno para hacer el Update!
+        Alumno a2 = new Alumno(1, 1234567, "suarez", "carlos", LocalDate.of(2001, 05, 25), true);
+        // alu.guardarAlumno(a1);
+        
+        //Metodo para modificar datos de un alumno
+        alu.modificarAlumno(a2);
     }
-    
+
 }
--- src/universidadejemploAccesoADAtos/AlumnoData.java
+++ src/universidadejemploAccesoADAtos/AlumnoData.java
@@ -21,31 +21,53 @@ import universidadejemplo.Entidades.Alumno;
  * @author veroc
  */
 public class AlumnoData {
+
     private Connection con = null;
-    public AlumnoData (){
-        con=Conexion.getConexion();
+
+    public AlumnoData() {
+        con = Conexion.getConexion();
     }
-    public void guardarAlumno(Alumno alumno){
+
+    public void guardarAlumno(Alumno alumno) {
         String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNacimiento, estado)"
                 + "VALUES (?,?,?,?,?)";
-        
+
         try {
-            try (PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS)) {
+            try (PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
                 ps.setInt(1, alumno.getDni());
                 ps.setString(2, alumno.getApellido());
                 ps.setString(3, alumno.getNombre());
                 ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
                 ps.setBoolean(5, alumno.isActivo());
                 ps.executeUpdate();
-                
-                ResultSet rs=ps.getGeneratedKeys();
-                if(rs.next()){
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
                     alumno.setIdAlumno(rs.getInt(1));
                     JOptionPane.showMessageDialog(null, "alumno agregado con exito");
                 }
             }
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "error al acceder a la tabla de alumno");
+        }
+    }
+
+    public void modificarAlumno(Alumno alumno) {
+        String sql = "UPDATE alumno SET dni=?, apellido=?, nombre=?, fechaNacimiento=?"
+                + "WHERE idAlumno = ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setInt(5, alumno.getIdAlumno());
+int exito = ps.executeUpdate();
+if (exito!=0){
+    JOptionPane.showMessageDialog(null, "alumno modificado");
+}
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "error " + ex);
         }
     }
 }
